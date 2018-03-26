ad531d6427e974972de71db9b3b3b3064f5209cc
Acytoo <acytoo@gmail.com>
2018-03-26 17:03:36 +0800

    一点点

--- AndroidClient/app/src/main/java/com/acytoo/hyperplanner/MainActivity.java
+++ AndroidClient/app/src/main/java/com/acytoo/hyperplanner/MainActivity.java
@@ -1,10 +1,14 @@
 package com.acytoo.hyperplanner;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -35,10 +39,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        String s="asdfas0oO1ilILdf";
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        Log.d("FirstActivity", s );
+        Log.i("asdfasdf",s);
+
+        Button ytbutton = (Button)findViewById(R.id.ytbutton);
+        ytbutton.setOnClickListener(
+                new Button.OnClickListener(){
+                    @Override
+                    public void onClick(View v) {
+                        Intent week =
+                    }
+                }
+        );
+
     }
+
+    public void haha(View view){
+        Log.d("click", "here");
+
+    }
+
+
 
 }
